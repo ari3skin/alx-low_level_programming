e7ff59ab84d9e0f1100447cadee5f041bb2171bc
Ryan <ryankinotikathurima@gmail.com>
2022-04-08 23:48:29 +0300

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - entry point
+ * description - print lowercase alphabet
+ * Return 0
+ */
+
+int main (void)
+{
+for (c == 0; c < 26; ++c)
+putchar ('a' + c);
+
+putchar('\n');
+
+return 0;
+}
