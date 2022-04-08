5bae6d80afdb9fa045d5c535d8046ffbbff2a347
Ryan <ryankinotikathurima@gmail.com>
2022-04-09 00:14:42 +0300

    Task  3 [correction 6]

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,17 +3,17 @@
  *main - entry point
  * 
  * description: print lower and upper cases
- * Return 0
+ * Return: Always 0
  */
 
 int main(void)
 {
 int c;
+
 for (c = 0; c < 26; ++c)
 putchar ('a' + c);
 for (c = 0; c < 26; ++c)
 putchar ('A' + c);
-
 putchar ('\n');
 
 return (0);
