91f6e61be027117fe775921d49db1d4e9eed1c64
Ryan <ryankinotikathurima@gmail.com>
2022-04-08 23:54:56 +0300

    Task 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio>
 /**
  * main - entry point
- * description - print lowercase alphabet
+ * description: print lowercase alphabet
  * Return 0
  */
 
-int main (void)
+int main(void)
 {
 for (c == 0; c < 26; ++c)
 putchar ('a' + c);
 
 putchar('\n');
 
-return 0;
+return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ *main - entry point
+ * 
+ * description: print lower and upper cases
+ * Return 0
+ */
+
+int main(void)
+{
+for (c = 0; c < 26; ++c)
+putchar ('a' + c);
+for (c = 0; c < 26; ++c)
+putchar ('A' + c);
+
+putchar ('\n');
+
+return (0);
+}
