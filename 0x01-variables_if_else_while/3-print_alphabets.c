3c27f08234da7014b753f7b3952538d8bfa14f25
Ryan Kin <ryankinotikathurima@gmail.com>
2022-04-12 08:04:34 +0300

    Task 3 [correction 1]

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,10 @@ int main(void)
 int c;
 
 for (c = 0; c < 26; ++c)
-putchar ('a' + c);
+putchar('a' + c);
 for (c = 0; c < 26; ++c)
-putchar ('A' + c);
-putchar ('\n');
+putchar('A' + c);
+putchar('\n');
 
 return (0);
 }
